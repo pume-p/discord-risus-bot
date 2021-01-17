4cb945fe86768d2e6a218e9382a7a1e63f1261de
pumegaming <pumepanarin@hotmail.com>
2021-01-17 22:24:22 +0700

    a

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ client.once('ready', () => {
     console.log('Ready!\n---');
     client.user.setActivity('risusiverse-thai.com/risus-bot');
     console.log(client.guilds.cache.get('716807791574253599').channels.cache.get('716815120663183390').permissionOverwrites);
+    console.log(client.guilds.cache.get('579966180681580544').channels.cache.get('789148404924153856').permissionOverwrites);
 });
 //! this code was orignally made for a single large server and is very very very old code 
 client.on('message', message => {
