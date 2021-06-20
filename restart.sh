063cd74b1ecb75d69dac5f6210de144d51e843bc
pumegaming <pumepanarin@hotmail.com>
2021-06-21 03:18:59 +0700

    #{ENV[

--- restart.sh
+++ restart.sh
@@ -20,4 +20,4 @@
 #  -H "Content-Type: application/json" \
  # -H "Accept: application/vnd.heroku+json; version=3" \
  # -H "Authorization: Bearer #{ENV["heroku_token"]}"
-curl -n -X DELETE https://api.heroku.com/apps/discord-risus-bot/dynos -H "Content-Type: application/json" -H "Accept: application/vnd.heroku+json; version=3" -H "Authorization: Bearer #{ENV["heroku_token"]}"
+curl -v -s -n -X DELETE https://api.heroku.com/apps/discord-risus-bot/dynos -H "Content-Type: application/json" -H "Accept: application/vnd.heroku+json; version=3" -H "Authorization: Bearer #{ENV["heroku_token"]}"
