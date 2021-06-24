3d22c3bfde94eafafac95f86e993d0ca04883772
pumegaming <pumepanarin@hotmail.com>
2021-06-24 12:35:09 +0700

    a

--- 24-7.js
+++ 24-7.js
@@ -1,33 +1,24 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 client.login(process.env.token_tester);
-
+const Heroku = require('heroku-client');
+const heroku = new Heroku({
+    token: process.env.heroku_token
+});
 client.once('ready', () => {
-    let filter = m => m.author.id === message.author.id
-    client.channels.cache.get('857483980973932604').send('!(1)').then(() => {
-      message.channel.awaitMessages(filter, {
-          max: 1,
-          time: 30000,
-          errors: ['time']
-        })
-        .then(message => {
-            client.channels.cache.get('857483980973932604').send('pass');
-        })
-        .catch(collected => {
-            request.delete(
-                {
-                    url: 'https://api.heroku.com/apps/discord-risus-bot/dynos',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'Accept': 'application/vnd.heroku+json; version=3',
-                        'Authorization': 'Bearer ' + process.env.heroku_token
-                    }
-                },
-                function(error, response, body) {
-                    // Do stuff
-                }
-            );
-        });
-    })
-
+    let filter = m => '766644729848528916' === '766644729848528916'
+    let ch = client.channels.cache.get('857483980973932604');
+    ch.send('!(1)').then(() => {
+        ch.awaitMessages(filter, {
+                max: 1,
+                time: 30000,
+                errors: ['time']
+            })
+            .then(() => {
+                ch.send('pass').then(() => client.destroy());
+            })
+            .catch(() => {
+                heroku.delete('https://api.heroku.com/apps/discord-risus-bot/dynos').then(() => client.destroy());
+            });
+    });
 });
